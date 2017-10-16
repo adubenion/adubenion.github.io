1d504f92c9f01849dad0bb510c97b8183b7a7e7d
adubenion <aaron.dubenion@gmail.com>
2017-10-16 12:28:12 -0400

    Upload Files

---
+++ App.js
@@ -0,0 +1,16 @@
+var destination = document.querySelector('#root');
+
+class App extends React.Component {
+	render() {
+		return(
+			<div>
+				<h1>Hello</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	destination
+);
